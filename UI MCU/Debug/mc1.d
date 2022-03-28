b6d4b655ce95f60750c7f9f1b95d8f327f7ecc3e
Mostafa Mahmoud <82781882+MostafaMahmoud54@users.noreply.github.com>
2022-03-28 16:42:28 +0200

    Add files via upload

---
+++ UI MCU/Debug/mc1.d
@@ -0,0 +1,18 @@
+mc1.o mc1.o: ../mc1.c ../lcd.h ../common_macros.h ../std_types.h \
+  ../keypad.h ../timer.h ../uart.h ../commands.h ../functions.h
+
+../lcd.h:
+
+../common_macros.h:
+
+../std_types.h:
+
+../keypad.h:
+
+../timer.h:
+
+../uart.h:
+
+../commands.h:
+
+../functions.h:
---
+++ UI MCU/commands.h
@@ -0,0 +1,28 @@
+/*
+ * commands.h
+ *
+ *  Created on: Nov 8, 2021
+ *      Author: Mostafa Mahmoud
+ */
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+#define MC2_READY 0x10
+#define received 0x01
+#define ready 0x02
+#define send 0x03
+#define REPASSWORD 0x04
+#define check 0x05
+#define result 0x06
+#define startTimer 0x08
+#define OpeningDoor 0x09
+#define ClosingDoor 0x0A
+#define DoorOpen 0x0B
+#define HoldDoor 0x0C
+#define error 0x0D
+#define plus 0x0E
+#define minus 0x0F
+#define start 0x11
+
+#endif /* COMMANDS_H_ */
